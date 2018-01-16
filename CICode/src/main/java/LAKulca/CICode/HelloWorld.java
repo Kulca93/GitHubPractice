f718350e8de4eee940fdd6634453ed1a491f6bbd
Kulca93 <Lee.Kulcsar@gmail.com>
2018-01-16 14:58:54 +0000

    firstcommit

---
+++ CICode/src/main/java/LAKulca/CICode/HelloWorld.java
@@ -0,0 +1,15 @@
+package LAKulca.CICode;
+
+public class HelloWorld {
+	
+public static void main(String[] args) {
+	
+	HelloWorld obj = new HelloWorld();
+	obj.printWord("HelloWorld!?!?");
+}
+
+	public void printWord(String text) {
+		System.out.println(text);
+	}
+}
+
